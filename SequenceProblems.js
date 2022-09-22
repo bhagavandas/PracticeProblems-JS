63c2084210b7dc6d67654e5fd51a206a1c89284d
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-09-22 08:38:00 +0530

    Added: Sequence Problems

---
+++ SequenceProblems.js
@@ -0,0 +1,26 @@
+// Pb:1
+let singleDigit = Math.floor(Math.random() * 10);
+console.log("Single Digit: " + singleDigit);
+// Pb:2
+let diceNum = Math.floor(Math.random() * 6) + 1;
+console.log("Dice Num: " + diceNum);
+// Pb:3
+let num = Math.floor(Math.random() * 6) + 1;
+let sum = diceNum + num;
+console.log("Sum of two random dice Numbers: " + sum);
+
+// Pb:4
+let num1 = Math.floor(Math.random() * 100);
+let num2 = Math.floor(Math.random() * 100);
+let num3 = Math.floor(Math.random() * 100);
+let num4 = Math.floor(Math.random() * 100);
+let num5 = Math.floor(Math.random() * 100);
+let sumofNum = num1 + num2 + num3 + num4 + num5;
+let avg = sumofNum / 5;
+console.log("Sum of Numbers: " + sumofNum + "   Average of Numbers: " + avg );
+
+// Pb:5
+var feet = 12;
+var inches = 42/feet;
+console.log("42 inches in Feets: " + inches);
+
